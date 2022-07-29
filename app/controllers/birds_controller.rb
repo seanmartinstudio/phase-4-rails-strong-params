8fa325f4f5802d721757528d37cfd37cd920bf55
Sean Martin <sean@seanmartinstudio.com>
2022-07-29 13:33:56 -0700

    Complete

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,12 +6,6 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
-  # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
-
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -20,6 +14,14 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  private
+  
+  # POST /birds
+  def create
+    bird = Bird.create(params.permit(:name, :species))
+    render json: bird, status: :created
   end
 
 end
